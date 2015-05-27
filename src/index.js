ee2b240f5f1391f77abfee58ce1b5b09a333c0f7
David Dias <daviddias.p@gmail.com>
2015-05-27 01:33:04 +0100

    add codestyle and precommit hook

--- src/index.js
+++ src/index.js
@@ -1,123 +1,119 @@
-var SimplePeer = require('simple-peer');
-var url = require('url');
-var request = require('request');
+var SimplePeer = require('simple-peer')
+var url = require('url')
+var request = require('request')
 
-exports = module.exports;
+exports = module.exports
 
-exports.createServer = Server;
+exports.createServer = Server
 
-function Server(callback) {
-    if (typeof window !== 'undefined') {
-        return callback(new Error('Can not create server on the browser'));
-    } 
+function Server (callback) {
+  if (typeof window !== 'undefined') {
+    return callback(new Error('Can not create server on the browser'))
+  }
 
-    if (!(this instanceof Server)) {
-        return new Server(callback);
-    }
-   
-    var wrtc = require('wrtc');
+  if (!(this instanceof Server)) {
+    return new Server(callback)
+  }
 
-    var self = this;
+  var wrtc = require('wrtc')
 
-    self.listen = function(port, ip) {
-        self.freshServer = true;  
-        var http = require('http');
-        var httpServer = http.createServer();
-       
-        httpServer.on('request', function(req, res) {
-            
-            // CORS should not be done this way :)
-            var origin = req.headers.origin
-            res.setHeader('Access-Control-Allow-Origin', origin)
+  var self = this
 
-            res.setHeader('Access-Control-Allow-Methods',
-                'POST, OPTIONS')
-            res.setHeader('Access-Control-Allow-Credentials', 'true')
-            res.setHeader('Access-Control-Max-Age', '86400')
-            res.setHeader('Access-Control-Allow-Headers',
-                'X-Requested-With, Content-Type, Accept')
+  self.listen = function (port, ip) {
+    self.freshServer = true
+    var http = require('http')
+    var httpServer = http.createServer()
 
-            if (req.method === 'OPTIONS') {
-                return res.end()
-            }
+    httpServer.on('request', function (req, res) {
+      // CORS should not be done this way :)
+      var origin = req.headers.origin
+      res.setHeader('Access-Control-Allow-Origin', origin)
 
-        });
+      res.setHeader('Access-Control-Allow-Methods',
+        'POST, OPTIONS')
+      res.setHeader('Access-Control-Allow-Credentials', 'true')
+      res.setHeader('Access-Control-Max-Age', '86400')
+      res.setHeader('Access-Control-Allow-Headers',
+        'X-Requested-With, Content-Type, Accept')
 
+      if (req.method === 'OPTIONS') {
+        return res.end()
+      }
 
-        self.attach(httpServer);
-                  
-        httpServer.listen(port, ip || '127.0.0.1');
-    };
+    })
 
-    self.attach = function (httpServer) {
-        httpServer.on('request', function(req, res) {
-            var path = url.parse(req.url).pathname;
-            if (req.method === 'POST' && path === '/webrtc-connect') {
-                
-                var peer = new SimplePeer({ 
-                    initiator: false, 
-                    trickle: false,
-                    wrtc: wrtc
-                });
+    self.attach(httpServer)
 
-                peer.on('signal', function(offer) {
-                    res.writeHead(200, {'Content-Type': 'application/json'});
-                    res.write(JSON.stringify(offer), 'utf8');
-                    res.end()
-                });
-                
-                peer.on('connect', function(){
-                    callback(null, peer);
-                });
+    httpServer.listen(port, ip || '127.0.0.1')
+  }
 
-                var data = '';
-                
-                req.on('data', function (chunk) {
-                    data += chunk.toString();
-                });
-
-                req.on('end', function () {
-                    peer.signal(JSON.parse(data));
-                });
-            } else if (self.freshServer && req.method !== 'OPTIONS') {
-                res.writeHead(404);
-                res.end();
-            }
-        }); 
-    };
-}
-
-exports.connect = function Client(opts, callback) {
-  
-    var wrtc;
-    if (typeof window === 'undefined') {
-        wrtc = require('wrtc');
-    } 
-
-    var peer = new SimplePeer({
-        initiator: true,
-        trickle: false,
-        wrtc: wrtc
-    });
-
-    peer.on('signal', function(signal){
-        request({
-            body: JSON.stringify(signal),
-            method: 'POST',
-            url: opts.url + ':' + (opts.port || 80) + '/webrtc-connect',
-            headers: {
-                "Content-Type": "application/json"
-            }
-        }, function (err, resp, body) {
-            if (err) {
-                return callback(err, null);
-            }
-            peer.signal(body);
+  self.attach = function (httpServer) {
+    httpServer.on('request', function (req, res) {
+      var path = url.parse(req.url).pathname
+      if (req.method === 'POST' && path === '/webrtc-connect') {
+        var peer = new SimplePeer({
+          initiator: false,
+          trickle: false,
+          wrtc: wrtc
         })
 
-    });
+        peer.on('signal', function (offer) {
+          res.writeHead(200, {'Content-Type': 'application/json'})
+          res.write(JSON.stringify(offer), 'utf8')
+          res.end()
+        })
 
-    peer.on('connect', function() {
-        callback(null, peer);
-    });
-};
+        peer.on('connect', function () {
+          callback(null, peer)
+        })
+
+        var data = ''
+
+        req.on('data', function (chunk) {
+          data += chunk.toString()
+        })
+
+        req.on('end', function () {
+          peer.signal(JSON.parse(data))
+        })
+      } else if (self.freshServer && req.method !== 'OPTIONS') {
+        res.writeHead(404)
+        res.end()
+      }
+    })
+  }
+}
+
+exports.connect = function Client (opts, callback) {
+  var wrtc
+  if (typeof window === 'undefined') {
+    wrtc = require('wrtc')
+  }
+
+  var peer = new SimplePeer({
+    initiator: true,
+    trickle: false,
+    wrtc: wrtc
+  })
+
+  peer.on('signal', function (signal) {
+    request({
+      body: JSON.stringify(signal),
+      method: 'POST',
+      url: opts.url + ':' + (opts.port || 80) + '/webrtc-connect',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }, function (err, resp, body) {
+      if (err) {
+        return callback(err, null)
+      }
+      peer.signal(body)
+    })
+
+  })
+
+  peer.on('connect', function () {
+    callback(null, peer)
+  })
+}
